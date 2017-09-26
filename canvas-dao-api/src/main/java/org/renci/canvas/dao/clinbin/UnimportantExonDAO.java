5cd4b178f33580c68201dc40cd6f90159548ddbc
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:13 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinbin/UnimportantExonDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.clinbin;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinbin.model.UnimportantExon;
-import org.renci.canvas.dao.clinbin.model.UnimportantExonPK;
-
-public interface UnimportantExonDAO extends BaseDAO<UnimportantExon, UnimportantExonPK> {
-
-}

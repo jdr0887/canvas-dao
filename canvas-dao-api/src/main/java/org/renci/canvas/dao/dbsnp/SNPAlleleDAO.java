07e5d22712654bf86834678e98355a726cc5b611
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:14 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/dbsnp/SNPAlleleDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.dbsnp;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.dbsnp.model.SNPAllele;
-import org.renci.canvas.dao.dbsnp.model.SNPAllelePK;
-
-public interface SNPAlleleDAO extends BaseDAO<SNPAllele, SNPAllelePK> {
-
-}

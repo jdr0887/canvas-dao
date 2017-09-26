2df1d9f7b4b196137007fa77a9b2f0dd75f0c836
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:13 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinbin/ReportDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.clinbin;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinbin.model.Report;
-import org.renci.canvas.dao.clinbin.model.ReportPK;
-
-public interface ReportDAO extends BaseDAO<Report, ReportPK> {
-
-}

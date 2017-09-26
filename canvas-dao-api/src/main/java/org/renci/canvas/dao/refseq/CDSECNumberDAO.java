3b3a9139a33f1e34b05b29cc22bf76fb272e0cdd
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:15 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/refseq/CDSECNumberDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.refseq;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.refseq.model.CDSECNumber;
-import org.renci.canvas.dao.refseq.model.CDSECNumberPK;
-
-public interface CDSECNumberDAO extends BaseDAO<CDSECNumber, CDSECNumberPK> {
-
-}

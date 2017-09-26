27b6f12535f8b245d33d431733a6da5364a8cb80
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:14 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinvar/TraitSetDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.clinvar;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinvar.model.TraitSet;
-
-public interface TraitSetDAO extends BaseDAO<TraitSet, Integer> {
-
-    
-}

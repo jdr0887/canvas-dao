850186d3040b7226b22c7f463bb22b968d67cd10
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:13 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinbin/AnalysisNoteDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.canvas.dao.clinbin;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinbin.model.AnalysisNote;
-
-public interface AnalysisNoteDAO extends BaseDAO<AnalysisNote, Integer> {
-
-}

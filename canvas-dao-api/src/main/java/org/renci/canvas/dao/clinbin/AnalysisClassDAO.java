d466fcf339cd2da52f388779986b2da1f7969379
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:13 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinbin/AnalysisClassDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.canvas.dao.clinbin;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinbin.model.AnalysisClass;
-
-public interface AnalysisClassDAO extends BaseDAO<AnalysisClass, Integer> {
-
-}

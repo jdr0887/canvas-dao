d770c5173bf800e6c81bead9acaec76ad98d9956
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:15 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/refseq/CDSTranslationExceptionDAO.java
+++
@@ -1,9 +0,0 @@
-package org.renci.canvas.dao.refseq;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.refseq.model.CDSTranslationException;
-import org.renci.canvas.dao.refseq.model.CDSTranslationExceptionPK;
-
-public interface CDSTranslationExceptionDAO extends BaseDAO<CDSTranslationException, CDSTranslationExceptionPK> {
-
-}

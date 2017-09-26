c2e8d7a3fa2e37f882a22f8f95147bc5b39c15ec
Jason Reilly <jdr0887@gmail.com>
2017-09-26 16:02:14 -0400

    removing

--- canvas-dao-api/src/main/java/org/renci/canvas/dao/clinvar/SubmissionClinicalAssertionDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.canvas.dao.clinvar;
-
-import org.renci.canvas.dao.BaseDAO;
-import org.renci.canvas.dao.clinvar.model.SubmissionClinicalAssertion;
-
-public interface SubmissionClinicalAssertionDAO extends BaseDAO<SubmissionClinicalAssertion, Long> {
-
-}
